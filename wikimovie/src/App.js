a3eb2fe05145c1bda6ac47428b329d68997e7c63
Manuel Zavala <majezanu@gmail.com>
2020-02-21 21:21:36 -0600

    fix: move Navbar to Home

--- wikimovie/src/App.js
+++ wikimovie/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/molecules/Navbar/Navbar';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faVideo } from '@fortawesome/free-solid-svg-icons'
 import RouterSwitch from './components/organisms/Router/Router';
@@ -9,7 +8,7 @@ library.add(faVideo);
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
       <div className='App-header'>
           <RouterSwitch></RouterSwitch>
       </div>
